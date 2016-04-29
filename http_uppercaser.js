500a09409b4b9e42ebf5c8b812930444b17943e3
Alan Baird <abaird@bairdsnet.net>
2016-04-29 22:41:35 +0000

    finshed http uppercaser

---
+++ http_uppercaser.js
@@ -0,0 +1,23 @@
+// Write an HTTP server that receives only POST requests and converts  
+// incoming POST body characters to upper-case and returns it to the client.  
+
+// Your server should listen on the port provided by the first argument to  
+// your program.  
+
+var http = require('http');
+var fs = require('fs');
+var map = require('through2-map');
+
+var port = Number(process.argv[2]);
+
+var server = http.createServer(function(req, res){
+  if (req.method !== 'POST')
+    return res.end('send me a POST\n');
+    
+  res.writeHead(200, { 'content-type': 'text/plain' });
+  req.pipe(map(function (chunk) {  
+    return chunk.toString().toUpperCase();  
+  })).pipe(res);
+});
+server.listen(port);
+
