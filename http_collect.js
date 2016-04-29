46b9d5eeb4e5b5dae1bcf47c1676e507580d7965
Alan Baird <abaird@bairdsnet.net>
2016-04-29 21:34:14 +0000

    update with help text

--- http_collect.js
+++ http_collect.js
@@ -26,6 +26,10 @@ var bl = require('bl');
 //     response.on('error', console.error);
 // });
 
+//  Both bl and concat-stream can have a stream piped in to them and they will  
+//  collect the data for you. Once the stream has ended, a callback will be  
+//  fired with the data:  
+
 http.get(url, function(response){
    response.pipe(bl(function (err, data){
        if (err)
